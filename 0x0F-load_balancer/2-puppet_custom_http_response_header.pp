a84e4b64cf961cec8a9e9343f9b8a3c5cec6bf76
Yvnn16 <yvonneweru5@gmail.com>
2023-04-10 19:56:34 +0000

    Tasks

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,36 @@
-# Custom HTTP header in a nginx server
-
-# update ubuntu server
-exec { 'update server':
-  command  => 'apt-get update',
-  user     => 'root',
-  provider => 'shell',
-}
-->
-# install nginx web server on server
+# puppet code to install & configure 'nginx'
 package { 'nginx':
-  ensure   => present,
-  provider => 'apt'
+  ensure => installed,
 }
-->
-# custom Nginx response header (X-Served-By: hostname)
-file_line { 'add HTTP header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;'
+
+file { '/etc/nginx/sites-available/default':
+  content => "server {
+                listen 80 default_server;
+                listen [::]:80 default_server;
+
+                root /var/www/html;
+                index index.html index.htm index.nginx-debian.html;
+
+                server_name _;
+
+                add_header X-Served-By $hostname;
+
+                location / {
+                        try_files $uri $uri/ =404;
+                }
+        }",
+  mode    => '0644',
+  require => Package['nginx'],
 }
-->
-# start service
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx']
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-enabled/default'],
 }
